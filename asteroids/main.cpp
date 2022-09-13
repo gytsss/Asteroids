2e42b0dbdd722a3bfe8eba8b07c8b4b3015542f8
gytsss <totogodoy@hotmail.com>
2022-09-13 12:53:59 -0300

    ball movement

--- asteroids/main.cpp
+++ asteroids/main.cpp
@@ -6,5 +6,27 @@ using namespace std;
 
 int main()
 {
-	cout << "hola";
+	InitWindow(1366, 768, "Asteroids");
+
+
+	Vector2 position{GetScreenWidth()/2 , GetScreenHeight()/2};
+
+
+	while (!WindowShouldClose())
+	{
+		if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
+		position = { GetMousePosition().x, GetMousePosition().y };
+
+
+		BeginDrawing();
+
+		ClearBackground(GREEN);
+		
+		DrawCircleV(position, 5, BLACK);
+
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
