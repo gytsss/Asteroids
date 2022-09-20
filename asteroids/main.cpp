cf60f28a0d33c4b7b5af762d33b9c1f93a577b1f
gytsss <totogodoy@hotmail.com>
2022-09-20 09:33:34 -0300

    add ship

--- asteroids/main.cpp
+++ asteroids/main.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include "raylib.h"
+#include "spaceShip.h"
 
 using namespace std;
 
+Ship ship;
 
 int main()
 {
-	InitWindow(1366, 768, "Asteroids");
+	float screenWidht = 1024;
+	float screenHeight = 768;
+
+	InitWindow(screenWidht, screenHeight, "Asteroids");
 
 
-	Vector2 position{GetScreenWidth()/2 , GetScreenHeight()/2};
+	Texture2D shipSprite = LoadTexture("assets/bluespaceship.png");
 
+	Vector2 mousePosition;
+
+	Rectangle nave = { GetScreenWidth() / 2, GetScreenHeight() / 2 , 15, 8};
+	Vector2 pivot = {nave.x + 7.5, nave.y + 4};
+
+	initShip(ship);
 
 	while (!WindowShouldClose())
 	{
-		if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
-		position = { GetMousePosition().x, GetMousePosition().y };
+		mousePosition = GetMousePosition();
 
+		//ship.position = {(float) GetScreenWidth() / 2, (float)GetScreenHeight() / 2 };
+
+		//Vector2 vectorDirection = {ship.position - mousePosition};
+
+		HideCursor();
+
+
+		if (CheckCollisionPointCircle(mousePosition, Vector2{ ship.position.x + 26, ship.position.y + 26 }, ship.radius))
+		{
+			ship.position = { (float)GetRandomValue(90, 1000), (float)GetRandomValue(90, 700) };
+
+		}
 
 		BeginDrawing();
+		ClearBackground(BLACK);
 
-		ClearBackground(GREEN);
-		
-		DrawCircleV(position, 5, BLACK);
+
+		DrawCircleLines(ship.position.x + 26, ship.position.y + 26, ship.radius, WHITE);
+		DrawRectanglePro(nave, pivot, 0, RED);
+		///*DrawTextureEx(shipSprite, ship.position, 0, 1, WHITE);
+		//*/DrawTextureEx(shipSprite, ship.position, 90, 1, WHITE);
+		DrawPixelV(mousePosition, WHITE);
+
 
 
 		EndDrawing();
 	}
+
+	//UnloadTexture(shipSprite);
 
 	CloseWindow();
 }
